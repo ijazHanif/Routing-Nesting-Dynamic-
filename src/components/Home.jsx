26a948825771128c14b06fa7ee3e9ae8d8ba37ed
ijaz Hanif <115060355+ijazHanif@users.noreply.github.com>
2024-08-26 14:26:07 +0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className=' bg-red-400 text-center text-white'>
+    <div className=' bg-red-400 text-md py-3 text-center text-white'>
        Home Page
     </div>
   )
